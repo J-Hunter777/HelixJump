2f44c60291f5fc8ec7f19a8063d1d81576bfc324
Julia <hunter-777@yandex.ru>
2024-07-31 13:38:05 +0300

    Create Tower Generation

--- TowerGeneration.cs
+++ TowerGeneration.cs
@@ -1,20 +1,148 @@
+using Assets.Scripts.Extentions;
 using Assets.Scripts.Platforms;
+using Assets.Scripts.Structur;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Tutorials.Core.Editor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.Tower
+    
+{
+
+    public class TowerGenerator : MonoBehaviour
+    {
+        [SerializeField] public TowerGenerationSettings _generetionSettings;
+
+        [Header("Tower")]
+        [SerializeField] private Transform _tower;
+
+        private FloatRange RotationRange => _generetionSettings.RotationRange;
+
+        private void Start()
+        {
+            Generate(_generetionSettings, _tower);
+        }
+        private void Generate(TowerGenerationSettings generetionSettings, Transform tower)
+        {
+            List<Platform> spawnedPlatforms = SpawnPlatforms(generetionSettings, out float offsetFromTop);
+
+            FitTowerHeight(tower, offsetFromTop);
+            spawnedPlatforms.ForEach(platform => platform.transform.SetParent(tower));
+        }
+
+        private List<Platform> SpawnPlatforms(TowerGenerationSettings generetionSettings, out float offsetFromTop)
+        {
+            offsetFromTop = generetionSettings.OffsetBetweenPlatforms;
+            const int startAndLastPlatforms = 2;
+            var spawnedPlatforms = new List<Platform>(generetionSettings.PlatformVariationCount + startAndLastPlatforms);
+
+            Platform startPlatform = Create(generetionSettings.StartPlatformPrefab, RotationRange, ref offsetFromTop);
+            spawnedPlatforms.Add(startPlatform);
+
+            for (int i = 0; i < generetionSettings.PlatformVariationCount; i++)
+            {
+                Platform platform = Create(generetionSettings.StartPlatformPrefab, RotationRange, ref offsetFromTop);
+                spawnedPlatforms.Add(platform);
+            }
+
+            Platform finishPlatform = Create(generetionSettings.FinishPlatformPrefab, RotationRange, ref offsetFromTop);
+            spawnedPlatforms.Add(finishPlatform);
+
+            return spawnedPlatforms;
+        }
+
+            private Vector3 GetRAndomYRotation(FloatRange rotationRAnge) =>
+                Vector3.up * _generetionSettings.RotationRange.Random;
+
+            private Platform Create(Platform platformPrefab, FloatRange rotationRange, ref float offsetFromTop)
+            {
+                Platform createdPlatform = Instantiate(platformPrefab);
+                Transform platformTransform = createdPlatform.transform;
+                platformTransform.position = Vector3.down * offsetFromTop;
+                platformTransform.eulerAngles = GetRAndomYRotation(rotationRange);
+
+                offsetFromTop += platformTransform.localScale.y + _generetionSettings.OffsetBetweenPlatforms;
+                return createdPlatform;
+            }
+            
+           private void FitTowerHeight(Transform tower, float height)
+            {
+                Vector3 originalSize = tower.localScale;
+                float towerHeight = height / 2.0f;
+                tower.localScale = new Vector3(originalSize.x, y: towerHeight, originalSize.z);
+                tower.localPosition -= Vector3.up * towerHeight;
+            }
+        }
+    } 
+
+using Assets.Scripts.Platforms;
+using Assets.Scripts.Structur;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using static Assets.Scripts.Platforms.Platform;
 
 namespace Assets.Scripts.Tower
 {
-    internal class TowerGenerator : MonoBehaviour
+    [CreateAssetMenu(menuName = "ScriptableObjects/Tower/TowerGenerationSettings", fileName = "TowerGenerationSettings")]
+
+    public class TowerGenerationSettings : ScriptableObject
     {
-        [SerializeField] private Platform _startPlatform;
-        [SerializeField] private Platform _finishPlatform;
-        [SerializeField] private Platform[] _platformVariants = Array.Empty<Platform>();
+        [SerializeField][Min(0)] private int _platformVariationCount;
+        [SerializeField][Min(0.0f)] private float _offsetBetweenPlatforms;
 
+        [SerializeField] private FloatRange _rotationRange;
 
+        [Header("Platform Prefab")]
+        [SerializeField] private Platform _startPlatformPrefab;
+        [SerializeField] private Platform _finishPlatformPrefab;
+        [SerializeField] private Platform[] _platformVariantsPrefabs = Array.Empty<Platform>();
 
+        public int PlatformVariationCount => _platformVariationCount;
+        public float OffsetBetweenPlatforms => _offsetBetweenPlatforms;
+
+        internal Platform StartPlatformPrefab => _startPlatformPrefab;
+
+        internal Platform FinishPlatformPrefab => _finishPlatformPrefab;
+
+        internal Platform[] PlatformVariationsPrefab => _platformVariantsPrefabs;
+
+        public FloatRange RotationRange => _rotationRange;
     }
 }
+using UnityEngine;
+
+namespace Assets.Scripts.Structur
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Structur/FloatRange", fileName = "FloatRange")]
+    public class FloatRange : Range<float>
+    {
+        public float Random => UnityEngine.Random.Range(Min, Max);
+    }
+}
+
+using UnityEngine;
+
+namespace Assets.Scripts.Structur
+{
+
+    public class Range<T> : ScriptableObject
+    {
+        [SerializeField] private T _min;
+        [SerializeField] private T _max;
+
+        public T Min => _min;
+        public T Max => _max;
+    }
+}
+
+
+
