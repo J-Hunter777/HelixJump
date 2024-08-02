df6021a191b7f0591738ec5aafa0c0c8fc4d2292
Julia <hunter-777@yandex.ru>
2024-08-03 00:36:38 +0300

    Trail to ball

--- BallBounceMechanic.cs
+++ BallBounceMechanic.cs
@@ -42,3 +42,116 @@ namespace Assets.Scripts.Phyzics.Bounce
 
     }
 }
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Structur
+{
+    [System.Serializable]
+    public class Vector3Curves
+    {
+        [SerializeField] private AnimationCurve _xCurve;
+        [SerializeField] private AnimationCurve _yCurve;
+        [SerializeField] private AnimationCurve _zCurve;
+
+        public AnimationCurve XCurve => _xCurve;
+        public AnimationCurve YCurve => _yCurve;
+        public AnimationCurve ZCurve => _zCurve;
+
+    }
+
+}
+
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Phyzics.Bounce
+{
+    [System.Serializable]
+    public class BounceData
+    {
+        [SerializeField] private float _force;
+        [SerializeField] private float _maxHeight;
+
+       public float Force => _force;
+        public float MaxHeight => _maxHeight;
+    }
+}
+using Assets.Scripts.Phyzics;
+using Assets.Scripts.Phyzics.Bounce;
+using System;
+using UnityEngine;
+using Assets.Scripts.Structur;
+
+namespace Assets.Scripts.Ball
+{
+    public class BallBounce : MonoBehaviour
+    {
+        [Header("Required components")]
+        [SerializeField] private Rigidbody _rigibody;
+        [SerializeField] private Transform _mesh;
+
+        [Header("Settings"]
+        [SerializeField] private BounceData _bounceData;
+        [SerializeField] private Vector3Curves _scaleCurves;
+
+        private Bounce _bounce;
+        private BounceEffects _bounceEffects;
+
+        private Vector3 _meshInitialScale;
+        private void Start()
+        {
+            _bounce = new Bounce(_rigibody, _bounceData);
+            _bounceEffects = new BounceEffects(_rigibody, _bounceData, _scaleCurves);
+
+            _meshInitialScale = _mesh.localScale;
+        }   
+
+       private void FixedUpdate()
+        {
+            _bounce.ClampHeight();
+            _bounceEffects.ApplayUpwardsScaleTo(_mesh, _meshInitialScale);
+        }
+        private void OnCollisionEnter(Collision other)
+        {
+            _bounce.BounceOff(Vector3.up);
+        }
+
+    }
+}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Phyzics.Bounce
+{
+    public class Bounce
+    {
+        private readonly Rigidbody _rigidbody;
+        private readonly BounceData _data;
+      
+        public Bounce(Rigidbody rigidbody, BounceData data)
+        {
+            _rigidbody = rigidbody;
+            _data = data;            
+        }
+
+        public void BounceOff(Vector3 direction) => 
+            _rigidbody.AddForce(direction * _data.Force, ForceMode.VelocityChange);
+        public void ClampHeight() 
+        {
+            Vector3 velocity = _rigidbody.velocity;
+
+            _rigidbody.velocity = velocity.y >= 0.0f
+                ? Vector3.ClampMagnitude(velocity, _data.MaxHeight)
+                : velocity;
+        }
+
+    }
+}
+
+
+
