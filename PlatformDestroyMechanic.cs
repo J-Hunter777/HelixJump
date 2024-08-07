33f9ab0533b9dab0edff30176ce71a283ee53cba
Julia <hunter-777@yandex.ru>
2024-08-07 19:23:54 +0300

    created platform destroy script

--- PlatformDestroyMechanic.cs
+++ PlatformDestroyMechanic.cs
@@ -19,3 +19,31 @@ namespace Assets.Scripts.Platforms.Parts
 
     }
 }
+using Assets.Scripts.Ball;
+using Assets.Scripts.Platforms;
+using UnityEngine;
+
+namespace Assets.Scripts.Platforms
+{
+    [RequireComponent(typeof(Collider))]
+    public class PlatformPassTrigger : MonoBehaviour
+    {
+        private Platform _parentPlatform;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out BallCollisions _) == false)
+                return;
+            _parentPlatform.UnhookAllParts();
+            Destroy(gameObject);
+        }
+        private void OnValidate()
+        {
+            GetComponent<Collider>().isTrigger = true;
+        }
+        private void Start()
+        {
+            _parentPlatform = GetComponentInParent<Platform>();
+        }
+
+      
+}
