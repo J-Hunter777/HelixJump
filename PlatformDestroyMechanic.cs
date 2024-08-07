4c7c4f5ca2f2c8ae5d60fc8c4af845010ac4a145
Julia <hunter-777@yandex.ru>
2024-08-07 12:26:21 +0300

    Created Platform Parts Classes

---
+++ PlatformDestroyMechanic.cs
@@ -0,0 +1,21 @@
+namespace Assets.Scripts.Platforms.Parts
+{
+    public class PlatformSaveZone : PlatformPart { }
+}
+
+using UnityEngine;
+
+namespace Assets.Scripts.Platforms.Parts
+{
+    public class PlatformObstacle : PlatformPart { }
+}
+
+using UnityEngine;
+
+namespace Assets.Scripts.Platforms.Parts
+{
+    public abstract class PlatformPart : MonoBehaviour
+    {
+
+    }
+}
