f0890dc1ec13fd35234f18dc1679ef91908abe93
Julia <hunter-777@yandex.ru>
2024-09-17 17:23:43 +0300

    changes

--- Assets/Scripts/Input/Swipes/Swipe.cs
+++ Assets/Scripts/Input/Swipes/Swipe.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine;
 
-namespace Assets.Scripts.Input
+namespace Input.Swipes
 {
     public readonly struct Swipe
     {
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Input.Swipes;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Test : MonoBehaviour
+    {
+        [SerializeField] private InputSwipePanel _swipePanel;
+
+        private void OnEnable()
+        {
+            _swipePanel.Swiping += OnSwiping;
+        }
+
+        private void OnDisable()
+        {
+            _swipePanel.Swiping -= OnSwiping;   
+        }
+
+        private void OnSwiping(Swipe swipe)
+        {
+            transform.position += (Vector3)swipe.Delta;
+        }
+    }
+    
+}
